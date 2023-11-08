63e2c2daf7a7cc2e1aba71859a425ded0c001756
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-11-09 01:48:52 +0530

    add assetion to random on int

--- lib/src/int.dart
+++ lib/src/int.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 extension IntExtension on int {
   /// Returns a random number by setting the value as max(exlusive).
   int random() {
+    assert(this > 0,
+        "Integer value should be greater than 0 for generating random number.");
     final random = Random();
     final number = random.nextInt(this);
     return number;
