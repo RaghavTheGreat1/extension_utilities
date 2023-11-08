72e25a060599f66844331de2a90ef45a40acc273
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-11-09 01:45:27 +0530

    add more examples

--- example/extension_utils_example.dart
+++ example/extension_utils_example.dart
@@ -1,6 +1,13 @@
 import 'package:extension_utilities/extension_utilities.dart';
+import 'package:extension_utilities/src/int.dart';
+import 'package:extension_utilities/src/set.dart';
 
 void main() {
   final list = ["Raghav", "Joshi", "@RaghavTheGreat1"];
   print(list.random(start: 1, end: 2));
+
+  final set = {"Raghav", "Joshi", "@RaghavTheGreat1"};
+  print(set.randomOrNull());
+
+  print(5.random());
 }
